8a5002b7ba14aadc4187c2b9dbd1990ecae7bac1
Aisha-Abdullahi <boyaaisha@gmail.com>
2023-04-09 01:56:42 +0300

    fixed error

--- exercise.js
+++ exercise.js
@@ -2,12 +2,12 @@ function printAsyncName(name, callback) {
     setInterval(function () {
         console.log(name);
 
-        setInterval(function () {
-            console.log("Hello");
-            callback()
-        }, 1000)
 
     }, 2000)
+    setInterval(function () {
+        console.log("Hello");
+        callback()
+    }, 1000)
 }
 
 printAsyncName("Aisha", function () { })
